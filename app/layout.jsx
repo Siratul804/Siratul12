b659b0b728b4d92d90968a26026670e3378d0eb9
Siratul Islam <islamsiratul@gmail.com>
2024-08-23 20:35:55 +0600

    siratul12

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,17 @@ export const metadata = {
   title: "Siratul Islam",
   description:
     "I have the power to create tools and solutions that simplify & improve people's lives so I am focused on building Scalable Solutions. ",
+  metadataBase: new URL("https://siratul12.vercel.app/"),
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+      "de-DE": "/de-DE",
+    },
+  },
+  openGraph: {
+    images: "/opengraph-image.png",
+  },
 };
 
 export default function RootLayout({ children }) {
